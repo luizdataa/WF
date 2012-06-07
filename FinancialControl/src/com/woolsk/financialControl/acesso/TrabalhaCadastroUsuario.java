21f2c3f250545a7015295b703011f41705d4e0b3
luizdataa <luiz.alberto1723@gmail.com>
2012-06-07 15:23:01 -0300

    Efeftuados testes utilizando hibernate e jsf

--- FinancialControl/src/com/woolsk/financialControl/acesso/TrabalhaCadastroUsuario.java
+++ FinancialControl/src/com/woolsk/financialControl/acesso/TrabalhaCadastroUsuario.java
@@ -1,5 +1,19 @@
 package com.woolsk.financialControl.acesso;
 
-public class TrabalhaCadastroUsuario {
+import javax.faces.bean.ManagedBean;
 
+import com.woolsk.financialControl.bean.Usuario;
+
+@ManagedBean(name="cadastroUsuario")
+public class TrabalhaCadastroUsuario{
+	
+	private Usuario usuario = new Usuario();
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
 }
