9399a2d7f9c1fa785617cc392c7c54930eff59f1
luizdataa <luiz.alberto1723@gmail.com>
2012-06-06 21:45:22 -0300

    Hibernate Configurado

--- FinancialControl/src/com/woolsk/financialControl/teste/TesteBean.java
+++ FinancialControl/src/com/woolsk/financialControl/teste/TesteBean.java
@@ -1,9 +1,24 @@
 package com.woolsk.financialControl.teste;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class TesteBean {
 	
+	@Id	@GeneratedValue
+	private int id;
+	
 	private String nome;;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
